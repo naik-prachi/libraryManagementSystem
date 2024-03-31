e6d3a4279799de90c111afbba74623ef0af17513
bluemoon <prachinnaik24@gmail.com>
2024-03-31 12:21:50 +0530

    moved functions

--- admin/adminViewBooks.php
+++ admin/adminViewBooks.php
@@ -18,57 +18,7 @@ $user_data = check_login($con);     // to check whether the user is logged in
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Website</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        /* CSS styles for the sidebar */
-        .sidebar {
-            height: 100%;
-            width: 250px;
-            position: fixed;
-            top: 0;
-            left: 0;
-            background-color: #343a40;
-            /* Dark background color */
-            padding-top: 20px;
-            color: #fff;
-            /* Text color */
-            overflow-y: auto;
-            /* Enable scrolling if content exceeds height */
-        }
-
-        .sidebar ul {
-            list-style-type: none;
-            padding: 0;
-        }
-
-        .sidebar a {
-            display: block;
-            padding: 10px 15px;
-            text-decoration: none;
-            color: #fff;
-        }
-
-        .sidebar a:hover {
-            background-color: #495057;
-            /* Darker background color on hover */
-        }
-
-        /* CSS styles for the main content */
-        .content {
-            margin-left: 250px;
-            /* Adjust this value to match the width of your sidebar */
-            padding: 20px;
-        }
-
-        .profile-pic img {
-            border-radius: 50%;
-            /* Make the profile picture round */
-        }
-
-        h1 {
-            color: #495057;
-            /* Text color for the heading */
-        }
-    </style>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
